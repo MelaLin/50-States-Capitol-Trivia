64cc429413cdd54021746a188411c1f488abf7ae
MelaLin <35199407+MelaLin@users.noreply.github.com>
2018-01-07 12:30:14 -0800

    Add files via upload
    first checkin

---
+++ lambda/custom/question.js
@@ -0,0 +1,419 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    QUESTIONS_EN_US: [
+        {
+            'What is the capitol of Alabama?': [
+                'Montgomery',
+                'Alambama City',
+                'Aliceville',
+                'Hueytown',
+                'Indian Springs Village',
+                'Shubda',
+            ],
+        },
+        {
+            'What is the capitol of Alaska?': [
+                'Juneau',
+                'Escamo',
+                'Alaska city',
+                'Germany',
+                'Canada',
+                'Norway',
+                'France',
+            ],
+        },
+        {
+            'What is the capitol of Arizona?': [
+                'Phoenix',
+                'Alabaster',
+                'Evergreen',
+                'Minstix',
+            ],
+        },
+        {
+            'What is the capitol of Arkansas?': [
+                'Little Rock',
+                'Little Rocky',
+                'Pebbles',
+                'No way to tell',
+            ],
+        },
+        {
+            'What is the capitol of California?': [
+                'Sacramento',
+                'San Franscisco',
+                'Cupertino',
+                'Oakland',
+            ],
+        },
+        {
+            'What is the capitol of Colorado?': [
+                'Denver',
+                'Color',
+                'Colorado City',
+                'Delta',
+            ],
+        },
+        {
+            'What is the capitol of Connecticut?': [
+                'Hartford',
+                'Burl',
+                'Soles',
+                'Shelton',
+            ],
+        },
+        {
+            'What is the capitol of Delaware?': [
+                'Dover',
+                'Sears',
+                'Delaware City',
+                'Newark',
+            ],
+        },
+        {
+            'What is the capitol of Florida?': [
+                'Tallahassee',
+                'Orlando',
+                'Wyamy',
+                'Tampa',
+            ],
+        },
+        {
+            'What is the capitol of Georgia?': [
+                'Atlanta',
+                'Savannah',
+                'Marietta',
+                'Athens',
+            ],
+        },
+        {
+            'What is the capitol of Hawaii?': [
+                'Honolulu',
+                'Lapland',
+                'Korvatunturi',
+                'Ewa Beach',
+            ],
+        },
+        {
+            'What is the capitol of Idaho?': [
+                'Boise',
+                'Meridian',
+                'Nampa',
+                'Elves',
+            ],
+        },
+        {
+            'What is the capitol of Illinois?': [
+                'Springfield',
+                'Chicago',
+                'Naperville',
+                'Peoria',
+            ],
+        },
+        {
+            'What is the capitol of Indiana?': [
+                'Indianapolis',
+                'Fort Wayne',
+                'Bloomington',
+                'Carmel',
+            ],
+        },
+        {
+            'What is the capitol of Iowa?': [
+                'Des Moines',
+                'Iowa City',
+                'Ames',
+                'Davenport',
+            ],
+        },
+        {
+            'What is the capitol of Kansas?': [
+                'Topeka',
+                'Wichita',
+                'Kansas City',
+                'Overland Park',
+            ],
+        },
+        {
+            'What is the capitol of Kentucky?': [
+                'Frankford',
+                'Lousiville',
+                'Lexington',
+                'Owensboro',
+            ],
+        },
+        {
+            'What is the capitol of Louisiana?': [
+                'Baton Rouge',
+                'New Orleans',
+                'Shreveport',
+                'Lafayette',
+            ],
+        },
+        {
+            'What is the capitol of Maine?': [
+                'Augusta',
+                'Portland',
+                'Lewiston',
+                'South Portland',
+            ],
+        },
+        {
+            'What is the capitol of Maryland?': [
+                'Annapolis',
+                'Baltimore',
+                'Rockville',
+                'Frederick',
+            ],
+        },
+        {
+            'What is the capitol of Massachusetts?': [
+                'Boston',
+                'Cambridge',
+                'Worcester',
+                'Salem',
+            ],
+        },
+        {
+            'What is the capitol of Michigan?': [
+                'Lansing',
+                'Ann Arbor',
+                'Flint',
+                'Kalamazoo',
+            ],
+        },
+        {
+            'What is the capitol of Minnesota?': [
+                'St. Paul',
+                'Minneaspolis',
+                'Duluth',
+                'St. Cloud',
+            ],
+        },
+        {
+            'What is the capitol of Mississippi?': [
+                'Jackson',
+                'Biloxi',
+                'Golfport',
+                'Hattiesburg',
+            ],
+        },
+        {
+            'What is the capitol of Missouri?': [
+                'Jefferson City',
+                'Kansas City',
+                'St. Louis',
+                'Columbia',
+            ],
+        },
+        {
+            'What is the capitol of Montana?': [
+                'Helena',
+                'Billings',
+                'Bozeman',
+                'Great Falls',
+            ],
+        },
+        {
+            'What is the capitol of Nebraska?': [
+                'Lincoln',
+                'Omaha',
+                'Grand Islands',
+                'Kearney',
+            ],
+        },
+        {
+            'What is the capitol of Nevada?': [
+                'Carson City',
+                'Las Vegas',
+                'Reno',
+                'Henderson',
+            ],
+        },
+        {
+            'What is the capitol of New Hampshire?': [
+                'Concord',
+                'Manchester',
+                'Nashua',
+                'Portsmouth',
+                'Keene',
+                'Derry',
+            ],
+        },
+        {
+            'What is the capitol of New Jersey?': [
+                'Carson City',
+                'Newark',
+                'Jersey City',
+                'Hoboken',
+            ],
+        },
+        {
+            'What is the capitol of New Mexico?': [
+                'Santa Fe',
+                'Taos',
+                'Albuquerque',
+                'Roswel',
+            ],
+        },
+        {
+            'What is the capitol of New York?': [
+                'Albany',
+                'New York City',
+                'Buffalo',
+                'Rochester',
+            ],
+        },
+        {
+            'What is the capitol of North Carolina?': [
+                'Raleigh',
+                'Charlotte',
+                'Durham',
+                'Greensboro',
+            ],
+        },
+        {
+            'What is the capitol of North Dakota?': [
+                'Bismarck',
+                'Minot',
+                'Fargo',
+                'Grand Forks',
+            ],
+        },
+        {
+            'What is the capitol of Ohio?': [
+                'Columbus',
+                'Cleveland',
+                'Dayton',
+                'Youngstown',
+            ],
+        },
+        {
+            'What is the capitol of Oklahoma?': [
+                'Oklahoma City',
+                'Norman',
+                'Edmond',
+                'Stillwater',
+            ],
+        },
+        {
+            'What is the capitol of Oregon?': [
+                'Salem',
+                'Portland',
+                'Bend',
+                'Beaverton',
+            ],
+        },
+        {
+            'What is the capitol of Pennsylvania?': [
+                'Harrisburg',
+                'York',
+                'Erie',
+                'Scranton',
+            ],
+        },
+        {
+            'What is the capitol of Rhode Island?': [
+                'Providence',
+                'Warwick',
+                'Newport',
+                'Woonsocket',
+            ],
+        },
+        {
+            'What is the capitol of South Carolina?': [
+                'Columbia',
+                'Charleston',
+                'Greenville',
+                'Spartanburg',
+            ],
+        },
+        {
+            'What is the capitol of South Dakota?': [
+                'Pierre',
+                'Sioux Falls',
+                'Rapid City',
+                'Brookings',
+            ],
+        },
+        {
+            'What is the capitol of Tennessee?': [
+                'Nashville',
+                'Memphis',
+                'Knoxville',
+                'Franklin',
+            ],
+        },
+        {
+            'What is the capitol of Texas?': [
+                'Austin',
+                'Dallas',
+                'Houston',
+                'El Paso',
+            ],
+        },
+        {
+            'What is the capitol of Utah?': [
+                'Salt Lake City',
+                'Provo',
+                'Ogden',
+                'Moab',
+            ],
+        }, 
+        {
+            'What is the capitol of Vermont?': [
+                'Montpelier',
+                'Burlington',
+                'Rutland City',
+                'Brattleboro',
+            ],
+        }, 
+        {
+            'What is the capitol of Virginia?': [
+                'Richmond',
+                'Charlottesville',
+                'Norfolk',
+                'Roanoke',
+            ],
+        },
+        {
+            'What is the capitol of Washington?': [
+                'Olympia',
+                'Seattle',
+                'Spokane',
+                'Bellevue',
+            ],
+        },
+        {
+            'What is the capitol of West Virginia?': [
+                'Charleston',
+                'Morgantown',
+                'Huntington',
+                'Wheeling',
+            ],
+        },
+        {
+            'What is the capitol of Wisconsin?': [
+                'Madison',
+                'Milwaukee',
+                'Green Bay',
+                'Appleton',
+            ],
+        },
+        {
+            'What is the capitol of Wyoming?': [
+                'Cheyenne',
+                'Casper',
+                'Jackson',
+                'Cody',
+            ],
+        },
+
+    ],
+ 
+};
